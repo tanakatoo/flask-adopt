a0aaeb3133602ea0bb8b678eb296ff2ab41230b8
Karmen Too <karmen.tanaka@gmail.com>
2022-11-01 14:02:34 -0400

    not detecting css file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, redirect, render_template, flash, ses
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql:///outer_space'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql:///pets'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
@@ -11,3 +11,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS']= False
 debug=DebugToolbarExtension(app)
 
 connect_db(app)
+
+@app.route('/')
+def home():
+    return render_template('listing.html')
--- models.py
+++ models.py
@@ -4,3 +4,22 @@ def connect_db(app):
     db.app=app
     db.init_app(app)
     app.app_context().push()
+    
+class Pet(db.Model):
+    
+    def __repr__(self):
+        return f"<id={self.id}, name={self.name}, species{self.species}, photo_url={self.photo_url}, age={self.age},notes={self.notes},available={self.available}>"
+    
+    id= db.Column(db.Integer,
+                  primary_key=True,
+                  autoincrement=True)
+    name=db.Column(db.Text,
+                   nullable=False)
+    species=db.Column(db.Text,
+                      nullable=False)
+    photo_url=db.Column(db.Text)
+    age=db.Column(db.Integer)
+    notes=db.Column(db.Text)
+    available=db.Column(db.Boolean, 
+                        nullable=False,
+                        default=True)
